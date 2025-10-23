8bc5514f32f7677c55c688157755dc617348ae68
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-10-22 23:16:13 -0500

    Refactor Intro.py layout and update content
    Updated layout and content for the introduction section.

--- Intro.py
+++ Intro.py
@@ -82,10 +82,9 @@ with col3:
  url = "https://vision2-gpt4o.streamlit.app/"
  st.write(f"Vision: [Enlace]({url})")
 
-# Segunda fila de columnas (vacías para que agregues luego tus imágenes y enlaces)
-col4, col5 = st.columns(2)
+col1, col2 = st.columns(2)
 
-with col4:
+with col1:
  st.subheader("Intro")
  image = Image.open('txt_to_audio2.png')
  st.image(image, width=190)
@@ -107,24 +106,24 @@ with col4:
  url = "https://traductoridioma.streamlit.app"
  st.write(f"YOLO: [Enlace]({url})")
 
-with col5:
- st.subheader("Intro")
- image = Image.open('txt_to_audio2.png')
+with col2: 
+ st.subheader("Conversión de voz a texto")
+ image = Image.open('OIG8.jpg')
+ st.image(image, width=200)
+ st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
+ url = "https://traductor-ab0sp9f6fi.streamlit.app/"
+ st.write(f"Voz a texto: [Enlace]({url})")
+
+ st.subheader("Reconocimiento Óptico de Caracteres OCR")
+ image = Image.open('data_analisis.png')
  st.image(image, width=190)
- st.write("En el siguiente enlase se vera la primera pagina") 
- url = "https://primerpaginacami.streamlit.app"
- st.write(f"Intro: [Enlace]({url})")
+ st.write("En la siguiente enlace veremos como se pueden analizar datos usando agentes.") 
+ url = "https://audio-ocr.streamlit.app"
+ st.write(f"Datos: [Enlace]({url})")
 
- st.subheader("Texto a Voz")
- image = Image.open('txt_to_audio.png')
+ st.subheader("Analisis de texto - Español")
+ image = Image.open('OIG3.jpg')
  st.image(image, width=200)
- st.write("En la siguiente enlace se vera la creacion de texto a voz") 
- url = "https://cuentodelgato.streamlit.app/"
- st.write(f"YOLO: [Enlace]({url})")
-
- st.subheader("Traductors")
- image = Image.open('OIG5.jpg')
- st.image(image, width=200)
- st.write("En la siguiente enlace se vera como traduce un audio.") 
- url = "https://traductoridioma.streamlit.app"
- st.write(f"YOLO: [Enlace]({url})")
+ st.write("En la siguiente enlace veremos como realizamos transcripciones de audio/video.") 
+ url = "https://txespanol.streamlit.app"
+ st.write(f"Transcriptor: [Enlace]({url})")
