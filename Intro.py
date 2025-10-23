fa5b2f25d5d0336acbe2a142812449bf5f4604a6
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-10-22 23:53:06 -0500

    Update text descriptions and images in Intro.py

--- Intro.py
+++ Intro.py
@@ -49,14 +49,14 @@ with col2:
  st.subheader("Reconocimiento Óptico de Caracteres OCR")
  image = Image.open('data_analisis.png')
  st.image(image, width=190)
- st.write("En la siguiente enlace veremos como se pueden analizar datos usando agentes.") 
+ st.write("En la siguiente enlace se vera el reconocimiento de caracteres en una foto.") 
  url = "https://audio-ocr.streamlit.app"
  st.write(f"Datos: [Enlace]({url})")
 
  st.subheader("Analisis de texto - Español")
  image = Image.open('OIG3.jpg')
  st.image(image, width=200)
- st.write("En la siguiente enlace veremos como realizamos transcripciones de audio/video.") 
+ st.write("Aqui se vera el analisis de texto en español") 
  url = "https://txespanol.streamlit.app"
  st.write(f"Transcriptor: [Enlace]({url})")
 
@@ -64,21 +64,21 @@ with col3:
  st.subheader("Analisis de texto - Ingles")
  image = Image.open('Chat_pdf.png')
  st.image(image, width=190)
- st.write("En la siguiente veremos una aplicación que usa RAG a partir de un documento (PDF).") 
+ st.write("Aqui se vera el analisis de texto en ingles") 
  url = "https://txingles.streamlit.app"
  st.write(f"RAG: [Enlace]({url})")
 
  st.subheader("Objetos en imagen")
  image = Image.open('OIG4.jpg')
  st.image(image, width=200)
- st.write("En la siguiente enlace veremos la capacidad de análisis en Imágenes.") 
+ st.write("Reconocimiento de objetos en imagenes") 
  url = "https://yolocami.streamlit.app"
  st.write(f"Vision: [Enlace]({url})")
  
  st.subheader("Reconocimiento de gestos")
  image = Image.open('OIG6.jpg')
  st.image(image, width=200)
- st.write("En la siguiente enlace veremos la capacidad de interacción con el mundo físico.") 
+ st.write("Aqui hay reconocimiento de gestos") 
  url = "https://tmcami.streamlit.app"
  st.write(f"Vision: [Enlace]({url})")
 
@@ -88,42 +88,42 @@ with col4:
  st.subheader("Chad PDF")
  image = Image.open('txt_to_audio2.png')
  st.image(image, width=190)
- st.write("En el siguiente enlase se vera la primera pagina") 
+ st.write("Analisis de documentos PDF") 
  url = "https://un-pdfchat.streamlit.app"
  st.write(f"Intro: [Enlace]({url})")
 
  st.subheader("Interpretacíon de imagen")
  image = Image.open('txt_to_audio.png')
  st.image(image, width=200)
- st.write("En la siguiente enlace se vera la creacion de texto a voz") 
+ st.write("Interpretac'ion de imagenes.") 
  url = "https://renocimientoimagenes.streamlit.app"
  st.write(f"YOLO: [Enlace]({url})")
 
  st.subheader("Tablero personalizado")
- image = Image.open('OIG5.jpg')
+ image = Image.open('image_2025-10-22_235245016.png')
  st.image(image, width=200)
- st.write("En la siguiente enlace se vera como traduce un audio.") 
+ st.write("Tablero personalizado de Caracteres.") 
  url = "https://tablerodibujoo.streamlit.app"
  st.write(f"YOLO: [Enlace]({url})")
 
 with col5: 
  st.subheader("Tablero Inteligente")
- image = Image.open('OIG8.jpg')
+ image = Image.open('image_2025-10-22_235148140.png')
  st.image(image, width=200)
- st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
+ st.write("Tablero inteligente que te contara que puede analisar.") 
  url = "https://bocetostablero.streamlit.app"
  st.write(f"Voz a texto: [Enlace]({url})")
 
  st.subheader("Lector de Sensor MQTT")
- image = Image.open('data_analisis.png')
+ image = Image.open('image_2025-10-22_235030401.png')
  st.image(image, width=190)
- st.write("En la siguiente enlace veremos como se pueden analizar datos usando agentes.") 
+ st.write("Sensor de MQTT.") 
  url = "https://recepprocess.streamlit.app"
  st.write(f"Datos: [Enlace]({url})")
 
  st.subheader("CONTROL POR VOZ")
- image = Image.open('OIG3.jpg')
+ image = Image.open('ctrlvoz.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace veremos como realizamos transcripciones de audio/video.") 
- url = "https://controlador-voz.streamlit.app"
+ url = "Control de voz con el Broker."
  st.write(f"Transcriptor: [Enlace]({url})")
