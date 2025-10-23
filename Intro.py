fbbefbe85b7bc3d9403c2e307ddb3b321d9527b1
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-10-22 23:30:26 -0500

    Update subheaders and URLs in Intro.py

--- Intro.py
+++ Intro.py
@@ -61,69 +61,69 @@ with col2:
  st.write(f"Transcriptor: [Enlace]({url})")
 
 with col3: 
- st.subheader("Generación en Contexto")
+ st.subheader("Analisis de texto - Ingles")
  image = Image.open('Chat_pdf.png')
  st.image(image, width=190)
  st.write("En la siguiente veremos una aplicación que usa RAG a partir de un documento (PDF).") 
- url = "https://chatpdf-cc.streamlit.app/"
+ url = "https://txingles.streamlit.app"
  st.write(f"RAG: [Enlace]({url})")
 
- st.subheader("Análisis de Imagen")
+ st.subheader("Objetos en imagen")
  image = Image.open('OIG4.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace veremos la capacidad de análisis en Imágenes.") 
- url = "https://vision2-gpt4o.streamlit.app/"
+ url = "https://yolocami.streamlit.app"
  st.write(f"Vision: [Enlace]({url})")
  
- st.subheader("Sistema Ciberfísico")
+ st.subheader("Reconocimiento de gestos")
  image = Image.open('OIG6.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace veremos la capacidad de interacción con el mundo físico.") 
- url = "https://vision2-gpt4o.streamlit.app/"
+ url = "https://tmcami.streamlit.app"
  st.write(f"Vision: [Enlace]({url})")
 
-col1, col2 = st.columns(2)
+col4, col5 = st.columns(2)
 
-with col1:
- st.subheader("Intro")
+with col4:
+ st.subheader("Chad PDF")
  image = Image.open('txt_to_audio2.png')
  st.image(image, width=190)
  st.write("En el siguiente enlase se vera la primera pagina") 
- url = "https://primerpaginacami.streamlit.app"
+ url = "https://un-pdfchat.streamlit.app"
  st.write(f"Intro: [Enlace]({url})")
 
- st.subheader("Texto a Voz")
+ st.subheader("Interpretacíon de imagen")
  image = Image.open('txt_to_audio.png')
  st.image(image, width=200)
  st.write("En la siguiente enlace se vera la creacion de texto a voz") 
- url = "https://cuentodelgato.streamlit.app/"
+ url = "https://renocimientoimagenes.streamlit.app"
  st.write(f"YOLO: [Enlace]({url})")
 
- st.subheader("Traductors")
+ st.subheader("Tablero personalizado")
  image = Image.open('OIG5.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace se vera como traduce un audio.") 
- url = "https://traductoridioma.streamlit.app"
+ url = "https://tablerodibujoo.streamlit.app"
  st.write(f"YOLO: [Enlace]({url})")
 
-with col2: 
- st.subheader("Conversión de voz a texto")
+with col5: 
+ st.subheader("Tablero Inteligente")
  image = Image.open('OIG8.jpg')
  st.image(image, width=200)
  st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
- url = "https://traductor-ab0sp9f6fi.streamlit.app/"
+ url = "https://bocetostablero.streamlit.app"
  st.write(f"Voz a texto: [Enlace]({url})")
 
- st.subheader("Reconocimiento Óptico de Caracteres OCR")
+ st.subheader("Lector de Sensor MQTT")
  image = Image.open('data_analisis.png')
  st.image(image, width=190)
  st.write("En la siguiente enlace veremos como se pueden analizar datos usando agentes.") 
- url = "https://audio-ocr.streamlit.app"
+ url = "https://recepprocess.streamlit.app"
  st.write(f"Datos: [Enlace]({url})")
 
- st.subheader("Analisis de texto - Español")
+ st.subheader("CONTROL POR VOZ")
  image = Image.open('OIG3.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace veremos como realizamos transcripciones de audio/video.") 
- url = "https://txespanol.streamlit.app"
+ url = "https://controlador-voz.streamlit.app"
  st.write(f"Transcriptor: [Enlace]({url})")
